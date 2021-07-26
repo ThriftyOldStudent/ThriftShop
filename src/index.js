f984d9c51d9c83578476ad95d44d04bce535bc32
ThriftyOldStudent <asvoria@live.com>
2021-07-27 01:16:37 +0800

    e

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const Web3 = require('web3')
 
 const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545')
 const getAccountsResults = document.getElementById('getAccountsResult')
-const contractAdds = '0x5faf7565A930d14657a17605C6E5675b57a6D246'
+const contractAdds = '0xC791BD709e29f6c60FaE2Cc8545F889681745754'
 const TOSadd = '0x8ff4E23dB50407Bf97Eb5acDC4b8E395E6a4dbf9'
 const TOSScontract = new web3.eth.Contract(TOSS_ABI, contractAdds)
 
