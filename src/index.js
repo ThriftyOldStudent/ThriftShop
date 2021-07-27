65fc8aeed10e63f7ddbf927b8c659ccefd1bdeb0
ThriftyOldStudent <asvoria@live.com>
2021-07-27 22:01:30 +0800

    s

--- src/index.js
+++ src/index.js
@@ -86,11 +86,11 @@ const convertImageToBase64 = async (imgURL) => {
     img.style.height = 'auto'
     img.crossOrigin = 'Anonymous'
 
-    const canvas = await document.createElement('canvas')
+    const canvas = document.createElement('canvas')
     canvas.width = img.width
     canvas.height = img.height
 
-    const ctx = await canvas.getContext('2d')
+    const ctx = canvas.getContext('2d')
     ctx.drawImage(img, 0, 0)
 
     img.style.width = originalWidth
@@ -105,11 +105,12 @@ const convertImageToBase64 = async (imgURL) => {
     ctx.font = '60px Arial'
     ctx.textAlign = 'center'
     ctx.fillStyle = 'blue'
-    await ctx.fillText('Item Paid!', 160, 80)
-    await ctx.fillText(datestr, 160, 160)
+    ctx.fillText('Item Paid!', 160, 80)
+    ctx.fillText(datestr, 160, 160)
 
     const dataUrl = await canvas.toDataURL('image/png')
 
+    return dataUrl
   }
 
   img.crossOrigin = 'anonymous'
